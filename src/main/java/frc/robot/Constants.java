63afab0b937e4c8f990162e85c56b4b7fe1a00c4
omer-irs <82238478+omer-irs@users.noreply.github.com>
2021-10-01 18:01:01 +0300

    Added port constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,4 +3,19 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-public final class Constants {}
+public final class Constants {
+	public static final int JOYSTICK = 0;
+	public static class Ports {
+		public static class Climb {
+			public static final int LEFT = 4;
+			public static final int RIGHT = 5;
+		}
+		public static class Drive {
+			public static final int LEFT = 1;
+			public static final int RIGHT = 0;
+		}
+	}
+	public static class JoystickButtons {
+
+	}
+}
